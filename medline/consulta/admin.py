5767b8ad10fac2df83e3228b49547381f14279b9
MarianaMikhael <marianamikhael@live.com>
2020-09-13 19:08:57 -0300

    Esboço de Login/Logout

--- medline/consulta/admin.py
+++ medline/consulta/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import *
+from consulta.models_login import User
+from consulta.models import *
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('role', 'username', 'get_full_name')
+    fields = (('is_active', 'is_staff', 'is_admin', 'is_superuser'), 'username', ('first_name', 'last_name'),
+        'email', 'date_joined', 'last_login')
+
+
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Pacientes)
 admin.site.register(Medicos)
 admin.site.register(Especialidades)
